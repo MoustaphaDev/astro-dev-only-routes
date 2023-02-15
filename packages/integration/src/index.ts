565735b8a8221bd3083988c70c0a756d4ae63545
Moustapha HappyDev <moustapha.happydev@gmail.com>
2023-02-15 09:58:30 +0000

    rename integration folder

---
+++ packages/integration/src/index.ts
@@ -0,0 +1,47 @@
+import type { AstroIntegration, AstroConfig } from 'astro';
+import { fileURLToPath } from 'url';
+import type { Plugin } from 'vite';
+import fs from 'fs';
+
+function getVitePlugin(config: AstroConfig): Plugin | void {
+    console.log('hey');
+    const devPreviewFolderPath = fileURLToPath(
+        new URL('./src/pages/__DEV_PREVIEW__', config.root)
+    );
+    // let doesFolderExist;
+    // try {
+    // } catch (e) {
+    //     return;
+    // }
+    return {
+        name: 'vite-plugin-astro-dev-preview-routes',
+        enforce: 'pre',
+        resolveId(id) {
+            console.log({ id, devPreviewFolderPath });
+            if (
+                !fs.existsSync(devPreviewFolderPath) ||
+                !id.startsWith(devPreviewFolderPath) ||
+                !id.endsWith('.astro')
+            ) {
+                console.log('nope');
+                return id;
+            }
+            return id.replace('__DEV_PREVIEW__', 'DEV_PREVIEW');
+        },
+    };
+}
+
+export default function integration(): AstroIntegration {
+    return {
+        name: 'astro-dev-preview-routes',
+        hooks: {
+            'astro:config:setup': ({ injectRoute, updateConfig, config }) => {
+                updateConfig({
+                    vite: {
+                        plugins: [getVitePlugin(config)],
+                    },
+                } as AstroConfig);
+            },
+        },
+    };
+}
