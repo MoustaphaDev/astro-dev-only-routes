19c18cda14c2b5bddaf4a80f3f0649156b0a9ef7
Moustapha HappyDev <moustapha.happydev@gmail.com>
2023-03-28 16:35:23 +0000

    dev only routes are working

--- packages/integration/src/index.ts
+++ packages/integration/src/index.ts
@@ -1,84 +1,60 @@
-import type { AstroIntegration, AstroConfig } from 'astro';
+import type { AstroIntegration } from 'astro';
 import { fileURLToPath } from 'url';
-import type { Plugin } from 'vite';
 import { globbySync } from 'globby';
-import npath from 'path';
+import kleur from 'kleur';
 
-const VIRTUAL_MODULE_ID = 'virtual:astro-dev-only-routes';
-const RESOLVED_VIRTUAL_MODULE_ID = '\0' + VIRTUAL_MODULE_ID;
-
-type PluginOptions = {
-    devOnlyRoutes?: string[];
-};
-function getVitePlugin(opts?: PluginOptions): Plugin | void {
-    console.log('hey');
-    if (!opts) return;
-    if (!opts.devOnlyRoutes) return;
-    const devOnlyRoutes = opts.devOnlyRoutes;
-    return {
-        name: 'vite-plugin-astro-dev-only-routes',
-        resolveId(id) {
-            const [requestId, query] = splitVirtualModuleRequest(id);
-
-            if (requestId === VIRTUAL_MODULE_ID) {
-                return RESOLVED_VIRTUAL_MODULE_ID + (query ? '?' + query : '');
-            }
-        },
-        load(id) {
-            const [, query] = splitVirtualModuleRequest(id);
-            if (id !== RESOLVED_VIRTUAL_MODULE_ID + (query ? '?' + query : ''))
-                return undefined;
-            // BUG?: returning undefined/null causes an error!!
-            if (!query) return '';
-
-            if (devOnlyRoutes.includes(query + '.astro')) return '';
-
-            return `import Component from "/src/pages/__${query}.astro"
-export default Component`;
-        },
-    };
-}
+// const pageExtRE = /\.(astro|mdx|md|tsx|ts|jsx|js)$/; Support later
+const pageExtRE = /\.astro$/;
 export default function integration(): AstroIntegration {
     return {
         name: 'astro-dev-only-routes',
         hooks: {
-            'astro:config:setup': ({ injectRoute, updateConfig, config }) => {
-                // Find a way to do this in the config:resolved hook, so that
-                // even injected dev only routes are included in the list.
-                // Right now some dev only routes may not be included in the list
-                // if they are injected after this integration
+            'astro:config:setup': ({ injectRoute, config, command }) => {
+                if (command === 'build') {
+                    return;
+                }
                 let pagesDir = new URL('./src/pages', config.root);
 
                 // TODO: support .mdx, .md, .ts, .js
-                let devOnlyRouteNames = globbySync('**/__*.astro', {
-                    cwd: pagesDir,
-                }).map((route) => {
-                    return route
-                        .slice('__'.length) // remove prefixed underscores
-                        .replace(/\.astro$/, ''); // remove .astro extension
+                let devOnlyRoutes = globbySync(
+                    ['**/__*.astro', '**/__*/**/*.astro'],
+                    {
+                        cwd: pagesDir,
+                    }
+                ).map((route) => {
+                    console.log({ route });
+                    const firstIndexOfDoubleUnderscore = route.indexOf('__');
+
+                    const pagesDirRelativePath = route.slice(
+                        0,
+                        firstIndexOfDoubleUnderscore
+                    );
+                    const routePath = route
+                        .slice(firstIndexOfDoubleUnderscore + '__'.length)
+                        .replace(pageExtRE, '');
+
+                    return { routePath, pagesDirRelativePath };
                 });
 
-                for (let route of devOnlyRouteNames) {
+                console.log({ devOnlyRoutes });
+
+                for (let { routePath, pagesDirRelativePath } of devOnlyRoutes) {
+                    if (routePath === 'index') {
+                        routePath = '';
+                    }
+                    const entryPoint = `${fileURLToPath(
+                        pagesDir
+                    )}/__${routePath}.astro`;
+                    const pattern = `/${pagesDirRelativePath}${routePath}`;
+
                     injectRoute({
-                        pattern: `/${route}`,
-                        entryPoint: `virtual:astro-dev-only-routes?${route}.astro`,
+                        entryPoint,
+                        pattern,
                     });
                 }
-
-                updateConfig({
-                    vite: {
-                        plugins: [getVitePlugin()],
-                    },
-                } as AstroConfig);
             },
         },
     };
-}
-
-function splitVirtualModuleRequest(request: string) {
-    const [id, ...rest] = request.split('?');
-    const query = rest.join('?');
-    return [id, query];
 }
 
 const dateTimeFormat = new Intl.DateTimeFormat([], {
@@ -87,25 +63,25 @@ const dateTimeFormat = new Intl.DateTimeFormat([], {
     second: '2-digit',
 });
 
-// function log(
-//     type: 'info' | 'warn' | 'error',
-//     message: string,
-//     /**
-//      * If true, don't log anything. Errors should not be silenced.
-//      */
-//     silent: boolean = false
-// ) {
-//     if (silent) return;
-//     const date = dateTimeFormat.format(new Date());
-//     const messageColor =
-//         type === 'error'
-//             ? kleur.red
-//             : type === 'warn'
-//             ? kleur.yellow
-//             : kleur.cyan;
-//     console.log(
-//         `${kleur.gray(date)} ${messageColor(
-//             kleur.bold('[astro-default-preprender]')
-//         )} ${messageColor(message)}`
-//     );
-// }
+function log(
+    type: 'info' | 'warn' | 'error',
+    message: string,
+    /**
+     * If true, don't log anything. Errors should not be silenced.
+     */
+    silent: boolean = false
+) {
+    if (silent) return;
+    const date = dateTimeFormat.format(new Date());
+    const messageColor =
+        type === 'error'
+            ? kleur.red
+            : type === 'warn'
+            ? kleur.yellow
+            : kleur.cyan;
+    console.log(
+        `${kleur.gray(date)} ${messageColor(
+            kleur.bold('[astro-default-preprender]')
+        )} ${messageColor(message)}`
+    );
+}
